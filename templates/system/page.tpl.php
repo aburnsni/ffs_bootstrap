fef17ab4a92165f9808064997605298bda09638b
Andrew Burns <aburnsni@gmail.com>
2019-02-11 14:52:58 +0000

    Removed header images

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -41,7 +41,7 @@
   </div>
 </header>
 
-<div class="main-container container">
+<div class="main-container container well">
 
   <header role="banner" id="page-header">
     <?php if (!empty($site_slogan)): ?>
@@ -65,29 +65,27 @@
     <?php endif; ?>
 
     <div<?php print $content_column_class; ?>>
-        <div class="well">
-            <?php if (!empty($page['highlighted'])): ?>
-                <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-            <?php endif; ?>
-            <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-            <a id="main-content"></a>
-            <?php print render($title_prefix); ?>
-            <?php if (!empty($title)): ?>
-                <h1 class="page-header"><?php print $title;?></h1>
-            <?php endif; ?>
-            <?php print render($title_suffix); ?>
-            <?php print $messages; ?>
-            <?php if (!empty($tabs)): ?>
-                <?php print render($tabs); ?>
-            <?php endif; ?>
-            <?php if (!empty($page['help'])): ?>
-                <?php print render($page['help']); ?>
-            <?php endif; ?>
-            <?php if (!empty($action_links)): ?>
-                <ul class="action-links"><?php print render($action_links); ?></ul>
-            <?php endif; ?>
-            <?php print render($page['content']); ?>
-        </div>
+      <?php if (!empty($page['highlighted'])): ?>
+          <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+      <?php endif; ?>
+      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if (!empty($title)): ?>
+          <h1 class="page-header"><?php print $title;?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php if (!empty($tabs)): ?>
+          <?php print render($tabs); ?>
+      <?php endif; ?>
+      <?php if (!empty($page['help'])): ?>
+          <?php print render($page['help']); ?>
+      <?php endif; ?>
+      <?php if (!empty($action_links)): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
     </div>
 
     <?php if (!empty($page['sidebar_second'])): ?>

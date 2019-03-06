50fab86850f5b60130a453ee76deb85b80ac5199
Andrew Burns <aburnsni@gmail.com>
2019-03-06 15:20:18 +0000

    Themeing changes to frontpage and beyond

--- layouts/2col/2col.tpl.php
+++ layouts/2col/2col.tpl.php
@@ -6,7 +6,7 @@
 ?>
 <?php !empty($css_id) ? print '<div id="' . $css_id . '">' : ''; ?>
 <?php if (!empty($content['top'])) : ?>
-  <div class="row">
+  <div class="row col2-top">
    <?php print $content['top']; ?>
   </div>
 <?php endif; ?>
@@ -14,20 +14,24 @@
     $panelcontent = array ($content['first'],$content['second'],$content['third'],$content['fourth']);
     $panelcontentfilled = array_values(array_filter($panelcontent));
   ?>
-  <div class="row">
+  <div class="row col2-mid">
     <div class="col-md-6 col-sm-12">
-      <?php if (@$panelcontentfilled[0]):
-        print $panelcontentfilled[0];
-        endif; ?>
+      <div class="well col2-mid-left">
+        <?php if (@$panelcontentfilled[0]):
+          print $panelcontentfilled[0];
+          endif; ?>
+      </div>
     </div>
     <div class="col-md-6 col-sm-12">
-      <?php if (@$panelcontentfilled[1]):
-        print $panelcontentfilled[1];
-        endif; ?>
+      <div class="well col2-mid-left">
+        <?php if (@$panelcontentfilled[1]):
+          print $panelcontentfilled[1];
+          endif; ?>
+      </div>
     </div>
   </div>
 <?php if (!empty($content['bottom'])) : ?>
-  <div class="row">
+  <div class="row col2-bottom">
     <?php print $content['bottom']; ?>
   </div>
 <?php endif; ?>

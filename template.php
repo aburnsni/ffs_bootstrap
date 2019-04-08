165cf377ea7b178bf6e6f8e8e57d0658b92f3800
Andrew Burns <aburnsni@gmail.com>
2019-04-08 21:01:00 +0100

    Removed duplicate meta tag

--- template.php
+++ template.php
@@ -3,3 +3,8 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+function fleming_html_head_alter(&$head_elements) {
+//  dsm($head_elements);
+  unset($head_elements['system_meta_content_type']);
+}

c0fc283f7dce47ef4b3227890de4c3e3290f5771
Andrew Burns <aburnsni@gmail.com>
2019-04-25 15:21:56 +0100

    Added class to image gallery captions

--- template.php
+++ template.php
@@ -8,3 +8,8 @@ function fleming_html_head_alter(&$head_elements) {
 //  dsm($head_elements);
   unset($head_elements['system_meta_content_type']);
 }
+
+function fleming_colorbox_node_caption($variables){
+  $markup     = "<div class=\"cnc-caption cnc-caption-${variables['location']} center-block\">${variables['caption']}</div>";
+  return ( $variables['location'] == 'before' ) ? $markup . $variables['item'] : $variables['item'] . $markup;
+}
